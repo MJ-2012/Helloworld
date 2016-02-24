03cbe13c2fe69237f3e45bf870e231c0182d9cf3
MJ-2012 <may@dingzai.com>
2016-02-24 11:14:51 +0800

    Added files via upload

---
+++ HellWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HellWorld
+//
+//  Created by MieJie on 16/2/24.
+//  Copyright © 2016年 MieJie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
